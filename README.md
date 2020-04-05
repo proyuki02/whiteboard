f4cb5168bc27970f536a0ea6ab0216a42f93f135
proyuki02 <proyuki02@gmail.com>
2020-04-06 03:08:16 +0900

    update

--- README.md
+++ README.md
@@ -1,17 +1,45 @@
+# Whiteboard
 
-# Socket.IO Collaborative Whiteboard
-
-A simple collaborative whiteboard for socket.io
+A simple collaborative whiteboard.
 
 ## How to use
 
 ```
-$ npm i && npm start
+$ npm i
+$ npm start
 ```
 
-And point your browser to `http://localhost:3000`. Optionally, specify
-a port by supplying the `PORT` env variable.
+And point your browser to `http://localhost:3000`.  
+Optionally, specify a port by supplying the `PORT` env variable.
+
+### Store data in Redis server
+
+Set env of Redis server to `REDIS_URL`.
+
+```
+$ docker run --name redis -d -p 6379:6379 redis redis-server --appendonly yes
+$ REDIS_URL=redis://localhost:6379 npm start
+```
+
+Optionally, specify the prefix for the key by specifying the `REDIS_PREFIX` env variable. (Default is `whiteboard-`)
+
+```
+$ REDIS_URL=redis://localhost:6379 REDIS_PREFIX=whiteboard- npm start
+```
 
 ## Features
 
-- draw on the whiteboard and all other users will see you drawings live
+- Works standalone.
+- Draw on the whiteboard and all other users will see you drawings live.
+- Sticky notes with signature support.
+- Redis support (optional).
+
+## References
+
+- [Socket.IO Collaborative Whiteboard](https://socket.io/demos/whiteboard/)
+- [Web Whiteboard](https://www.webwhiteboard.com)
+- [オンラインふせんアプリを作りたい人のためのファーストガイド - Qiita](https://qiita.com/iotas/items/fbf4994877e5c2053787)
+
+## License
+
+MIT
--- public/main.js
+++ public/main.js
@@ -1,106 +1,280 @@
-'use strict';
+"use strict";
 
-(function() {
+// config
+const PEN_WIDTH = 4;
+const ERASER_WIDTH = 50;
+const signatureFormat = (signature) => {
+  return signature.length > 0 ? `(${signature})` : "";
+};
 
-  var socket = io();
-  var canvas = document.getElementsByClassName('whiteboard')[0];
-  var colors = document.getElementsByClassName('color');
-  var context = canvas.getContext('2d');
+(function () {
+  const MENU_HEIGHT = 20;
+  const PADDING = 10;
 
-  var current = {
-    color: 'black'
+  const socket = io();
+  const current = {
+    x: 0,
+    y: 0,
+    color: "black",
+    width: PEN_WIDTH,
+    mode: "pen",
   };
-  var drawing = false;
+  setCursor();
+  let drawing = false;
 
-  canvas.addEventListener('mousedown', onMouseDown, false);
-  canvas.addEventListener('mouseup', onMouseUp, false);
-  canvas.addEventListener('mouseout', onMouseUp, false);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-  
+  const canvas = document.getElementById("whiteboard");
+  const context = canvas.getContext("2d");
+  context.lineJoin = "round";
+  context.lineCap = "round";
+
+  canvas.addEventListener("mousedown", onMouseDown, false);
+  canvas.addEventListener("mouseup", onMouseUp, false);
+  canvas.addEventListener("mouseout", onMouseUp, false);
+  canvas.addEventListener("mousemove", throttle(onMouseMove, 10), false);
+
   //Touch support for mobile devices
-  canvas.addEventListener('touchstart', onMouseDown, false);
-  canvas.addEventListener('touchend', onMouseUp, false);
-  canvas.addEventListener('touchcancel', onMouseUp, false);
-  canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+  canvas.addEventListener("touchstart", onMouseDown, false);
+  canvas.addEventListener("touchend", onMouseUp, false);
+  canvas.addEventListener("touchcancel", onMouseUp, false);
+  canvas.addEventListener("touchmove", throttle(onMouseMove, 10), false);
 
-  for (var i = 0; i < colors.length; i++){
-    colors[i].addEventListener('click', onColorUpdate, false);
-  }
+  $(".colors").click(onPenSelect);
+  $(".stickers").click(onStickerSelect);
+  $("#clear-button").click(onClearBourd);
 
-  socket.on('drawing', onDrawingEvent);
+  socket.on("drawLine", drawLine);
+  socket.on("updateNote", updateNote);
+  socket.on("deleteNote", deleteNote);
+  socket.emit("load", null, (data) => {
+    const { lineHist, noteList } = data;
+    for (let line of lineHist) {
+      drawLine(line);
+    }
+    for (let key of Object.keys(noteList)) {
+      updateNote(noteList[key]);
+    }
+  });
 
-  window.addEventListener('resize', onResize, false);
-  onResize();
-
-
-  function drawLine(x0, y0, x1, y1, color, emit){
+  function drawLine(data, emit) {
     context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.strokeStyle = color;
-    context.lineWidth = 2;
+    context.moveTo(data.x0 - PADDING, data.y0 - PADDING - MENU_HEIGHT);
+    context.lineTo(data.x1 - PADDING, data.y1 - PADDING - MENU_HEIGHT);
+    context.strokeStyle = data.color;
+    context.lineWidth = data.width;
     context.stroke();
     context.closePath();
 
-    if (!emit) { return; }
-    var w = canvas.width;
-    var h = canvas.height;
+    if (emit) {
+      socket.emit("drawLine", data);
+    }
+  }
 
-    socket.emit('drawing', {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color
+  function updateNote(data, emit) {
+    const { id, x, y, w, h, msg, color } = data;
+    let note = $(`#${id}`);
+    if (!note.length) {
+      note = $("#note-origin").clone();
+      note.attr("id", id);
+      note.removeClass("hidden");
+      note.draggable({
+        dragstart: () => emitNoteState(note),
+        drag: () => emitNoteState(note),
+        dragstop: () => emitNoteState(note),
+      });
+
+      const delButton = note.find(".delete-note");
+      delButton.click(() => {
+        deleteNote({ id }, true);
+      });
+
+      const textarea = note.find(".expanding");
+      autosize(textarea);
+      $("#sticky-note-container").append(note);
+      if (emit) {
+        setTimeout(() => {
+          textarea.focus();
+          const t = textarea.get(0);
+          t.selectionStart = 0;
+          t.selectionEnd = 0;
+        }, 0);
+      }
+    }
+    note.css({ left: x, top: y });
+    const textarea = note.find(".expanding");
+    textarea.val(msg);
+    textarea.css({ "background-color": color, width: w, height: h });
+    textarea.on("keyup mouseup mouseout mousemove touchend touchmove", () => {
+      emitNoteState(note);
     });
+
+    if (emit) {
+      socket.emit("updateNote", { id, x, y, w, h, msg, color });
+    }
   }
 
-  function onMouseDown(e){
-    drawing = true;
-    current.x = e.clientX||e.touches[0].clientX;
-    current.y = e.clientY||e.touches[0].clientY;
+  function emitNoteState(note) {
+    const id = note.attr("id");
+    const x = note.css("left");
+    const y = note.css("top");
+    const textarea = note.find(".expanding");
+    const w = textarea.css("width");
+    const h = textarea.css("height");
+    const msg = textarea.val();
+    const color = textarea.css("background-color");
+    socket.emit("updateNote", { id, x, y, w, h, msg, color });
   }
 
-  function onMouseUp(e){
-    if (!drawing) { return; }
+  function deleteNote(data, emit) {
+    const { id } = data;
+    const note = $(`#${id}`);
+    if (note.length) {
+      note.remove();
+    }
+    if (emit) {
+      socket.emit("deleteNote", { id });
+    }
+  }
+
+  function onMouseDown(e) {
+    current.x = e.pageX || e.touches[0].pageX;
+    current.y = e.pageY || e.touches[0].pageY;
+    if (current.mode === "pen") {
+      drawing = true;
+      $(".note").css("pointer-events", "none");
+    } else {
+      onNoteCreate();
+    }
+  }
+
+  function onMouseUp(e) {
+    if (!drawing) {
+      return;
+    }
     drawing = false;
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
+    $(".note").css("pointer-events", "auto");
+    drawLine(
+      {
+        x0: current.x,
+        y0: current.y,
+        x1: e.pageX || e.touches[0].pageX,
+        y1: e.pageY || e.touches[0].pageY,
+        color: current.color,
+        width: current.width,
+      },
+      true
+    );
   }
 
-  function onMouseMove(e){
-    if (!drawing) { return; }
-    drawLine(current.x, current.y, e.clientX||e.touches[0].clientX, e.clientY||e.touches[0].clientY, current.color, true);
-    current.x = e.clientX||e.touches[0].clientX;
-    current.y = e.clientY||e.touches[0].clientY;
+  function onMouseMove(e) {
+    if (!drawing) {
+      return;
+    }
+    drawLine(
+      {
+        x0: current.x,
+        y0: current.y,
+        x1: e.pageX || e.touches[0].pageX,
+        y1: e.pageY || e.touches[0].pageY,
+        color: current.color,
+        width: current.width,
+      },
+      true
+    );
+    current.x = e.pageX || e.touches[0].pageX;
+    current.y = e.pageY || e.touches[0].pageY;
   }
 
-  function onColorUpdate(e){
-    current.color = e.target.className.split(' ')[1];
+  function onPenSelect(e) {
+    const color = e.target.getAttribute("data-color");
+    const eraser = color === "white";
+    const width = eraser ? ERASER_WIDTH : PEN_WIDTH;
+    current.color = color;
+    current.width = width;
+    current.mode = "pen";
+    setCursor();
+  }
+
+  function onStickerSelect(e) {
+    const color = e.target.getAttribute("data-color");
+    current.color = color;
+    current.mode = "sticker";
+    setCursor();
+  }
+
+  function onNoteCreate() {
+    const color = current.color === "hotpink" ? "pink" : "lightyellow";
+    const id = "note-" + generateUuid();
+    const x = current.x - 80;
+    const y = current.y - 60;
+    const msg = signatureFormat($("#signature").val());
+    const w = $("#note-origin").css("width");
+    const h = $("#note-origin").css("height");
+    updateNote({ id, x, y, w, h, msg, color }, true);
+  }
+
+  function onClearBourd() {
+    if (confirm("CLEAR the board. Are you okay?")) {
+      socket.emit("onClearBourd", null, () => {
+        window.location.reload();
+      });
+    }
   }
 
   // limit the number of events per second
   function throttle(callback, delay) {
-    var previousCall = new Date().getTime();
-    return function() {
-      var time = new Date().getTime();
-
-      if ((time - previousCall) >= delay) {
+    let previousCall = new Date().getTime();
+    return function () {
+      const time = new Date().getTime();
+      if (time - previousCall >= delay) {
         previousCall = time;
-        callback.apply(null, arguments);
+        return callback.apply(null, arguments);
       }
     };
   }
 
-  function onDrawingEvent(data){
-    var w = canvas.width;
-    var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+  function setCursor() {
+    const mode = current.mode;
+    let color = current.color;
+    let unicode, size, tweakX, tweakY;
+    if (mode === "pen") {
+      if (color === "white") {
+        unicode = "\uf12d";
+        size = 48;
+        tweakX = 70;
+        tweakY = 30;
+        color = "black";
+      } else {
+        unicode = "\uf304";
+        size = 24;
+        tweakX = 25;
+        tweakY = 25;
+      }
+    } else {
+      unicode = "\uf249";
+      size = 24;
+      tweakX = 25;
+      tweakY = 25;
+    }
+
+    const canvas = document.createElement("canvas");
+    canvas.width = size * 2;
+    canvas.height = size * 2;
+
+    const context = canvas.getContext("2d");
+    context.font = `900 ${size}px "Font Awesome 5 Free"`;
+    context.fillStyle = color;
+    context.fillText(unicode, canvas.width / 2, canvas.width / 2);
+
+    $("#whiteboard").css(
+      "cursor",
+      `url(${canvas.toDataURL("image/png")}) ${tweakX} ${tweakY}, auto`
+    );
   }
 
-  // make the canvas fill its parent
-  function onResize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
-
+  const generateUuid = (
+    $ = (a, b) => (Math.floor(Math.random() * a) + b).toString(16)
+  ) =>
+    "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+      .replace(/x/g, (_) => $(16, 0))
+      .replace(/y/g, (_) => $(4, 8));
 })();
